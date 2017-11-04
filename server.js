469f7d52902180014cfddd137b8bc07a009fba23
Josh Daugherty <jd21.daugherty@gmail.com>
2017-11-04 11:12:40 -0500

    Hid api under /api

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Ritt example API!'))
+app.get('/api', (req, res) => res.send('Ritt example API!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
