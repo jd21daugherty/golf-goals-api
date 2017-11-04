0948cf790a2fe0b7ada3c5c96e7afe43df33fbe3
Josh Daugherty <jd21.daugherty@gmail.com>
2017-11-04 10:24:32 -0500

    Named server Golf Goals API

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Golf Goals API!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
