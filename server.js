f17621c6f266a3e0f87721093027f5d34ffd230d
Unknown <costello.ritt@gmail.com>
2017-11-18 08:28:25 -0600

    add bodyparser to server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
 
 const routes = require('./router');
 
-app.get('/', routes);
+// for POST request parsing
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use('/', routes);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
